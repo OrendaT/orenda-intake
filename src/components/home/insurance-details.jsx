5b3ebbc3b022a901fd816bf1823d1e3f914369f4
vehktaur <victorakhihiero@gmail.com>
2025-05-31 05:00:10 +0100

    refactor: make photo ID field optional in InsuranceDetails component

--- src/components/home/insurance-details.jsx
+++ src/components/home/insurance-details.jsx
@@ -35,7 +35,7 @@ const InsuranceDetails = () => {
         <h3 className='label'>
           Please upload a copy of a government or school-issued photo ID.{' '}
         </h3>
-        <FileInput label='Photo ID' name='photo_ID' />
+        <FileInput label='Photo ID' name='photo_ID' required={false} />
       </div>
     </section>
   );
