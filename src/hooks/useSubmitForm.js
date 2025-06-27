4b7b1290528925ddf3daf9bf77c4d9e23dfc0303
vehktaur <victorakhihiero@gmail.com>
2025-06-27 13:11:57 +0100

    feat: implement success modal for form submission and remove success page

--- src/hooks/useSubmitForm.js
+++ src/hooks/useSubmitForm.js
@@ -6,7 +6,9 @@ import { toast } from 'sonner';
 const useSubmitForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
   const [error, setError] = useState(null);
+  
 
   const submitForm = async (data) => {
     let response;
@@ -19,6 +21,7 @@ const useSubmitForm = () => {
       response = await axios.post('patients', formData);
       removeLSItem('form_id');
       setError(null);
+      setIsSuccess(true);
     } catch (error) {
       toast.error(error.response?.data?.message || 'Something went wrong');
       setIsError(true);
@@ -35,6 +38,6 @@ const useSubmitForm = () => {
     return response;
   };
 
-  return { isLoading, isError, error, submitForm };
+  return { isLoading, isError, isSuccess, error, submitForm };
 };
 export default useSubmitForm;
