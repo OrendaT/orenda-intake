f0e26b7dec74249a404128c174cf5e6728d89e6c
vehktaur <victorakhihiero@gmail.com>
2025-07-18 06:56:23 +0100

    update

--- src/routes/credit-card/-components/success-modal.tsx
+++ src/routes/credit-card/-components/success-modal.tsx
@@ -1,7 +1,6 @@
 import { XIcon } from 'lucide-react';
 import {
   AlertDialog,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogHeader,
@@ -21,13 +20,14 @@ const SuccessModal = ({ open }: { open: boolean }) => {
           </AlertDialogDescription>
         </AlertDialogHeader>
 
-        <AlertDialogCancel
+        <button
+          type='button'
           onClick={close}
           className='absolute top-4 right-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-zinc-100 data-[state=open]:text-zinc-500 dark:ring-offset-zinc-950 dark:focus:ring-zinc-300 dark:data-[state=open]:bg-zinc-800 dark:data-[state=open]:text-zinc-400'
         >
           <XIcon className='size-6' />
           <span className='sr-only'>Close</span>
-        </AlertDialogCancel>
+        </button>
 
         <div className='flex flex-col items-center py-8 text-center'>
           <SuccessIcon className='text-orenda-purple clamp-[size,20,32] mb-6' />
