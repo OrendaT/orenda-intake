fcdd2d46688e2aa75025739be5f81639a0b803b8
vehktaur <victorakhihiero@gmail.com>
2025-09-15 18:55:31 +0100

    update

--- src/components/hidden-section.tsx
+++ src/components/hidden-section.tsx
@@ -21,13 +21,13 @@ const HiddenSection = ({
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -10 }}
-          onAnimationComplete={() => {
-            // Scroll into view after the enter animation completes
-            ref.current?.scrollIntoView({
-              behavior: 'smooth',
-              block: 'center',
-            });
-          }}
+          // onAnimationComplete={() => {
+          //   // Scroll into view after the enter animation completes
+          //   ref.current?.scrollIntoView({
+          //     behavior: 'smooth',
+          //     block: 'center',
+          //   });
+          // }}
           className={cn(
             'clamp-[ml,1.5,5] relative mt-5 py-2 ps-5 pe-0 before:absolute before:inset-y-0 before:left-0 before:h-full before:w-[3px] before:rounded-full before:bg-[#b2b2b2]',
             className,
